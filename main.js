cc983d9125940a687c10b62d99770a218f94873b
TranTam <tamvaa1306@gmail.com>
2022-09-03 12:07:14 +0700

    init

---
+++ main.js
@@ -0,0 +1,13 @@
+const video = document.getElementById("video");
+
+function startVideo() {
+  navigator.getUserMedia(
+    {
+      video: {},
+    },
+    (stream) => (video.srcObject = stream),
+    (err) => console.error(err)
+  );
+}
+
+startVideo();
